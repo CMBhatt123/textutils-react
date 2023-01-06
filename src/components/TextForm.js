fb31f4ecda811bfafcf5b2a8a8697cbaa2725150
CMBhatt123 <Chandra.755421@snsc.tu.edu.np>
2023-01-06 13:26:03 +0545

    using hooks

---
+++ src/components/TextForm.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+export default function Textform(props) {
+  const [text, setText] = useState("Enter text here");
+  return (
+    <div>
+      <div className="mb-3">
+        <h1>{props.heading}</h1>
+        <label for="mybox" className="form-label"></label>
+        <textarea className="form-control my-3" id="mybox" rows="8"></textarea>
+      </div>
+      <button className="btn btn-primary">Convert to uppercase</button>
+    </div>
+  );
+}
