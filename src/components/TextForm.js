0bc046a6ba95b87aab3d895dc6621682a223eb1c
CMBhatt123 <Chandra.755421@snsc.tu.edu.np>
2023-01-06 14:52:18 +0545

    added first feature lowecase to uppercase

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
 export default function Textform(props) {
   const [text, setText] = useState("Enter text here");
+  const handleUpclick = () => {
+    console.log("Uppercase was clicked" + text);
+
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleOnChange = (event) => {
+    console.log("On Change");
+    setText(event.target.value);
+  };
   return (
     <div>
       <div className="mb-3">
-        <h1>{props.heading}-{text}</h1>
+        <h1>{props.heading}</h1>
         <label for="mybox" className="form-label"></label>
-        <textarea className="form-control my-3" id="mybox" rows="8"></textarea>
+        <textarea
+          className="form-control my-3"
+          value={text}
+          onChange={handleOnChange}
+          id="mybox"
+          rows="8"
+        ></textarea>
       </div>
-      <button className="btn btn-primary">Convert to uppercase</button>
+      <button className="btn btn-primary" onClick={handleUpclick}>
+        Convert to uppercase
+      </button>
     </div>
   );
 }
